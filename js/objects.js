c8587669741a67c61c90a3b0feb56c199f170d34
Jonathan Francisco Sanchez <jsanchez8@crpgp----------.global.publicisgroupe.net>
2014-09-28 22:48:37 -0600

    few dummy examples about how to create objects in javascript

---
+++ js/objects.js
@@ -0,0 +1,31 @@
+// how to create right objects
+
+// how to create a Doggy
+function Doggy(name, birthdate, color, breed) {
+
+    this.name = name;
+    this.birthdate = birthdate;
+    this.color = color;
+    this.breed = breed;
+   /*
+    this.bark = function () {
+
+        alert (this.name + 'rau rau');
+    }*/
+} // Doggy.
+
+Doggy.prototype.bark =  function () {
+
+    alert (this.name + ' rau rau rau');
+}
+
+var puppy = new Doggy("puppy", "01/01/2002", "white", "french poodle");
+var tita = new Doggy("tita", "01/01/2005", "white", "french poodle");
+
+//alert (puppy instanceof Doggy);
+//alert (tita instanceof Doggy);
+
+puppy.bark();
+tita.bark();
+
+alert((puppy.bark == tita.bark));
