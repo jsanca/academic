ce38fc6c1926cf73fff17cb2696a1f51cd230940
jonathan.sanchez <jonathanfrancisco.sanchez@razorfish.com>
2015-02-06 09:16:41 -0600

    starting with the classes about design pattern

---
+++ design_patterns_java/src/main/java/designpatterns/vo/dto/StudentDTO.java
@@ -0,0 +1,51 @@
+package designpatterns.vo.dto;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+/**
+ * Student VO/DTO
+ *
+ * See all the properties are physical types
+ *
+ * @author jsanca
+ */
+public class StudentDTO implements Serializable {
+
+    private String number;
+    private String name;
+    private Long   birthDate;
+    private Long   startDate;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Long birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Long getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Long startDate) {
+        this.startDate = startDate;
+    }
+}
