896518610fd5ee3ecd85fb574718f1aae6341ad2
Jonathan Francisco Sanchez <jsanchez8@CRSJOPGP0266.local>
2015-07-28 15:21:55 -0600

    Added some collections draft 2

--- collections/src/main/java/cr/jsanca/collections/map/MultiKeyHashMap.java
+++ collections/src/main/java/cr/jsanca/collections/map/MultiKeyHashMap.java
@@ -1,6 +1,5 @@
 package cr.jsanca.collections.map;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 /**
@@ -10,59 +9,67 @@ import java.util.*;
  */
 public class MultiKeyHashMap<K, V> extends HashMap<K, V> implements MultiKeyMap<K, V> {
 
-    private Map<K, List<K>> keyListMap = new HashMap<K, List<K>>();
+    private final Map<K, K> keyIndexMap = new HashMap<K, K>();
 
 
     @Override
     public MultiKeyMap addSynonyms(K key, V value, K... synonymKeys) {
 
-        // creates the list of keys
-        final ArrayList<K> keyList = new ArrayList<K>(Arrays.asList(synonymKeys));
-        keyList.add(key);
-
         // add the key/value replicated entries
         this.put(key, value);
-        this.keyListMap.put(key, keyList);
 
+        // index the synonyms
         if (null != synonymKeys) {
 
             for (K synonymKey : synonymKeys) {
 
-                this.put(synonymKey, value);
-                this.keyListMap.put(synonymKey, keyList);
+                this.keyIndexMap.put(synonymKey, key);
             }
         }
 
         return this;
     } // addSynonyms.
 
+    private K getFirstOne(final LinkedHashSet<K> keySet) {
+
+        return keySet.iterator().next();
+    }
+
     @Override
     public V put(K key, V value) {
 
-        final List<K> keyList = this.keyListMap.get(key);
+        // if it is a synonym, so use the main key
+        if (this.keyIndexMap.containsKey(key)) {
 
-        if (null != keyList) {
-
-            for (K synonymKey : keyList) {
-
-                super.put(synonymKey, value);
-            }
+            key = this.keyIndexMap.get(key);
         }
 
-        // redundant but not a big deal
         return super.put(key, value);
     } // put.
+
+    @Override
+    public V get(Object key) {
+
+        // if it is a synonym, so use the main key
+        if (this.keyIndexMap.containsKey(key)) {
+
+            key = this.keyIndexMap.get(key);
+        }
+
+        return super.get(key);
+    }
 
     public static void main(String[] args) {
 
         Map<String, Integer> multiKeyMap =
-                new MultiKeyHashMap<String, Integer>().addSynonyms("A", 0).addSynonyms("B", 0, "C");
+                new MultiKeyHashMap<String, Integer>().addSynonyms("A", 0).addSynonyms("B", 0, "C", "D");
 
         // Case 1
         final Map<String, Integer> map = new HashMap<String, Integer>();
         map.put("A", 50);
         map.put("B", 50);
         map.put("C", 50);
+        map.put("D", 50);
 
         sum(map, multiKeyMap);
 
@@ -70,6 +77,7 @@ public class MultiKeyHashMap<K, V> extends HashMap<K, V> implements MultiKeyMap<
         System.out.println("A = " + multiKeyMap.get("A"));
         System.out.println("B = " + multiKeyMap.get("B"));
         System.out.println("C = " + multiKeyMap.get("C"));
+        System.out.println("multiKeyMap = " + multiKeyMap);
 
         // Case 2
         map.clear();
@@ -84,6 +92,8 @@ public class MultiKeyHashMap<K, V> extends HashMap<K, V> implements MultiKeyMap<
         System.out.println("A = " + multiKeyMap.get("A"));
         System.out.println("B = " + multiKeyMap.get("B"));
         System.out.println("C = " + multiKeyMap.get("C"));
+        System.out.println("multiKeyMap = " + multiKeyMap);
+
 
         // Case 3
         map.clear();
@@ -95,6 +105,7 @@ public class MultiKeyHashMap<K, V> extends HashMap<K, V> implements MultiKeyMap<
         System.out.println("A = " + multiKeyMap.get("A"));
         System.out.println("B = " + multiKeyMap.get("B"));
         System.out.println("C = " + multiKeyMap.get("C"));
+        System.out.println("multiKeyMap = " + multiKeyMap);
     }
 
     private static void sum (Map<String, Integer> tableMap, Map<String, Integer> resultMap) {
